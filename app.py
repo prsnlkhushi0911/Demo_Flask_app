9a7c19b26b93d3ba2f67c9c5af28d72bbad05c71
prsnlkhushi0911 <prsnlkhushi0911@gmail.com>
2026-07-22 23:36:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Khushi, i integrated webhook, lets test, might work."
+    return "Hello from Khushi."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
